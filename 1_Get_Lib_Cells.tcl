cf66fae25e56c97bb9413dd7a31bd01db72596c4
Manish Mani <manishpamu@hotmail.com>
2025-01-10 01:08:53 +0530

    Update 1_Get_Lib_Cells.tcl

--- 1_Get_Lib_Cells.tcl
+++ 1_Get_Lib_Cells.tcl
@@ -1,5 +1,6 @@
 #! /bin/tclsh8.5
 
+
 puts -nonewline "Enter the search pattern"
 flush stdout
 gets stdin pattern
@@ -7,10 +8,11 @@ gets stdin pattern
 set x [get_object_name [get_lib_cells */$pattern*]]
 set Total_Count [sizeof_collection [get_lib_cells */$pattern*]]
 
-###---------------------------------------------------------------------------------->
+#############################################################################
 for {set n 0} {$n == [llength $x]} {incr n} {
 puts [lindex $x $n]
 }
 
 puts "Total items count: $Total_Count"
-###---------------------------------------------------------------------------------->
+#############################################################################
+
