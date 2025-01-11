772f2ad13dbc4cf069081f37c048fdb78adc102f
Manish Mani <manishpamu@hotmail.com>
2025-01-12 01:09:46 +0530

    Update 3_set.tcl

--- Concepts/3_set.tcl
+++ Concepts/3_set.tcl
@@ -1,10 +1,10 @@
 #Declaring varibale
-set a 10; puts $a;                         #Output: 10
-set a APPLE; puts $a;                      #Output: APPLE
-set a {APPLE BANANA}; puts $a;             #Output: APPLE BANANA
-set a {0 1 2 3 4}; puts $a;                #Output: 0 1 2 3 4
-set a [list 0 1 2 3 4]; puts $a;           #Output: 0 1 2 3 4
+set a 10; puts $a                          ; #Output: 10
+set a APPLE; puts $a                       ; #Output: APPLE
+set a {APPLE BANANA}; puts $a              ; #Output: APPLE BANANA
+set a {0 1 2 3 4}; puts $a                 ; #Output: 0 1 2 3 4
+set a [list 0 1 2 3 4]; puts $a            ; #Output: 0 1 2 3 4
 
 #nested list having 5 items, 4th item is again a list of 2 items
-set var {a b c {x y} d}; puts $var;          #Output: a b c {x y} d
+set var {a b c {x y} d}; puts $var         ; #Output: a b c {x y} d
 
