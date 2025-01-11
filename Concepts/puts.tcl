40e869cc6f6fdca8841db45f635264bbe4ef7e89
Manish Mani <manishpamu@hotmail.com>
2025-01-11 16:36:59 +0530

    NA

--- Concepts/puts.tcl
+++ Concepts/puts.tcl
@@ -1,1 +1,17 @@
+######################################################################
+
+#Command:
 puts "Hello World"
+#Output:
+Hello World
+
+######################################################################
+
+#Command:
+puts stdout "Hello World"
+#Output:
+Hello World
+
+######################################################################
+
+#it doesn't matter if we mention "stdout" or not in puts command, because it will take that by default
