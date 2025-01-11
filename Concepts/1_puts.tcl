9da1225d92b8a42d5056308a69ba5b52035e1b5a
Manish Mani <manishpamu@hotmail.com>
2025-01-12 01:08:51 +0530

    Update 1_puts.tcl

--- Concepts/1_puts.tcl
+++ Concepts/1_puts.tcl
@@ -1,7 +1,8 @@
-puts "Hello World";              #Output: Hello World
-puts stdout "Hello World";       #Output: Hello World
 #it doesn't matter if we mention "stdout" or not in puts command, default option for puts is "stdout".
 
-set a RED; puts $a;        #Output: RED
-set a RED; puts "$a";      #Output: RED
-set a RED; puts {$a};      #Output: $a      #works for escape char
+puts "Hello World"                     ; #Output: Hello World
+puts stdout "Hello World"              ; #Output: Hello World
+
+set a RED; puts $a                     ; #Output: RED
+set a RED; puts "$a"                   ; #Output: RED
+set a RED; puts {$a}                   ; #Output: $a       {} - works for escape char
