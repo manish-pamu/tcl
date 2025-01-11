af9cfed7d46b44bec801453514fd83c1e7b21386
Manish Mani <manishpamu@hotmail.com>
2025-01-11 16:55:13 +0530

    Update puts.tcl

--- Concepts/puts.tcl
+++ Concepts/puts.tcl
@@ -1,15 +1,4 @@
-#Command:
-puts "Hello World"
-#Output:
-Hello World
-
-#---------------------------------------------------------------------#
-
-#Command:
-puts stdout "Hello World"
-#Output:
-Hello World
-
-#---------------------------------------------------------------------#
+puts "Hello World";              #Output: Hello World
+puts stdout "Hello World";       #Output: Hello World
 
 #it doesn't matter if we mention "stdout" or not in puts command, default option for puts is "stdout".
