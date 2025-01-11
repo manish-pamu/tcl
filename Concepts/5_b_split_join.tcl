013a2d090a468f3b5cda7834a8134c3bf566e520
Manish Mani <manishpamu@hotmail.com>
2025-01-12 01:11:04 +0530

    Update 5_b_split_join.tcl

--- Concepts/5_b_split_join.tcl
+++ Concepts/5_b_split_join.tcl
@@ -1,5 +1,5 @@
 set x {x1 x2 x3 x4};          # x is having 4 items seperated by a space
 set y {y1_y2_y3_y4};          # y is having 4 items seperated by an underscore (or) a single item seperated by a space
 
-puts [join $x ""];              #Output: x1x2x3x4
-puts [split $y "_"];            #Output: y1 y2 y3 y4
+puts [join $x ""];            #Output: x1x2x3x4
+puts [split $y "_"];          #Output: y1 y2 y3 y4
