5ed02e4fa1b741d6237687117ced575f3a0b1a14
Manish Mani <manishpamu@hotmail.com>
2025-01-11 21:22:54 +0530

    Create 4_Find_Clocks.tcl

---
+++ TCL Scripts/4_Find_Clocks.tcl
@@ -0,0 +1,13 @@
+#! /bin/tclsh8.5
+
+#AIM: To find the clocks in design
+foreach i [get_object_name [all_registers -clock_pins]] {
+        
+        set a [get_object_name [all_fanin -to $i -flat -startpoints_only]]
+        if {[llength $a] >= 0} {
+                foreach j [get_object_name [get_ports [all_inputs]]] {
+                if {$j == $a} {lappend c $a}
+                }
+        }
+}
+puts "[lsort -u $c]"
