baa987cb41868c77892ad988e07fcd5021b1b0bc
Manish Mani <manishpamu@hotmail.com>
2025-01-12 01:10:52 +0530

    Update 5_a_lindex.tcl

--- Concepts/5_a_lindex.tcl
+++ Concepts/5_a_lindex.tcl
@@ -3,13 +3,13 @@ set var {apple banana kiwi {blue_berry black_berry straw_berry} lemon papaya}
 # (or) 
 set var [list [apple banana kiwi [list blue_berry black_berry straw_berry] lemon papaya]]
 
-puts [lindex $var 0];                #Output: apple
-puts [lindex $var 1];                #Output: banana
-puts [lindex $var 3 0];              #Output: blue_berry
-puts [lindex $var 3 1];              #Output: black_berry
-puts [lindex $var 4];                #Output: lemon
+puts [lindex $var 0];                              #Output: apple
+puts [lindex $var 1];                              #Output: banana
+puts [lindex $var 3 0];                            #Output: blue_berry
+puts [lindex $var 3 1];                            #Output: black_berry
+puts [lindex $var 4];                              #Output: lemon
 
-puts [lindex $var end];              #Output: papaya
-puts [lindex $var end-1];            #Output: lemon
+puts [lindex $var end];                            #Output: papaya
+puts [lindex $var end-1];                          #Output: lemon
 
-puts [lindex $var [expr [llength $var] - 1]]      #Output: papaya
+puts [lindex $var [expr [llength $var] - 1]];      #Output: papaya
