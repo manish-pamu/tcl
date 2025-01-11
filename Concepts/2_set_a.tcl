fe70bb7d7e48c0ad7e27ae1d89e643f363f81e1f
Manish Mani <manishpamu@hotmail.com>
2025-01-11 22:21:40 +0530

    Update 2_set_a.tcl

--- Concepts/2_set_a.tcl
+++ Concepts/2_set_a.tcl
@@ -5,6 +5,6 @@ set a {APPLE BANANA}; puts $a;             #Output: {APPLE BANANA}
 set a {0 1 2 3 4}; puts $a;                #Output: {0 1 2 3 4}
 set a [list 0 1 2 3 4]; puts $a;           #Output: {0 1 2 3 4}
 
-#nested list having 4 items, 4th item is a list of 2 items
+#nested list having 4 items, 4th item is again a list of 2 items
 set var {a b c {x y}}; puts $var;          #Output: {a b c {x y}}
 
