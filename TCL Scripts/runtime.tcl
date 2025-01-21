7f132d8629fd52d0afb394984789b23ab0a95c8e
Manish Mani <manishpamu@hotmail.com>
2025-01-22 00:20:23 +0530

    Create runtime.tcl

---
+++ TCL Scripts/runtime.tcl
@@ -0,0 +1,11 @@
+#record start time
+set start_time [clock clicks -milliseconds]
+
+#run the command example: wait for 5 seconds
+
+
+#record end time
+set start_time [clock clicks -milliseconds]
+
+#calc the runtime/elapsed time
+puts "Elapsed time: [] ms"
