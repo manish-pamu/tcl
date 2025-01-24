66a745f9d951c1afcba0c5dee55c0b12cf44f523
Manish Mani <manishpamu@hotmail.com>
2025-01-24 12:28:00 +0530

    Create 7_Elapsed_time_v2.tcl

---
+++ TCL Scripts/7_Elapsed_time_v2.tcl
@@ -0,0 +1,11 @@
+#Read a script file having commands and
+#To print elapsed time after each command executes
+
+set lines [open "rndm_commands.tcl" r]
+set n 1
+
+while {[gets $lines command] > 0} {
+
+
+
+}
