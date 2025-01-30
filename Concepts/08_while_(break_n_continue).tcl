6364766453a0ff96e87ec4b30fc86f03007c1c55
Manish Mani <manishpamu@hotmail.com>
2025-01-30 23:09:08 +0530

    Update 08_while_(break_n_continue).tcl

--- Concepts/08_while_(break_n_continue).tcl
+++ Concepts/08_while_(break_n_continue).tcl
@@ -6,7 +6,7 @@ while {$j <= 5} {
 
 	set k 1
  	while {$k <= 11} {
-	if {$k == 11} { incr k; puts ""; break } else { puts "$j x $k = [expr $j * $k]"; incr k }
+	if {$k == 11} { incr k;   puts "";   break } else { puts "$j x $k = [expr $j * $k]";   incr k }
 	}
 
 incr j
