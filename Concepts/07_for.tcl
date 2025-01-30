0387d212446c8ff00e07f18784f433d42f4f7424
Manish Mani <manishpamu@hotmail.com>
2025-01-30 19:28:16 +0530

    Update 07_for.tcl

--- Concepts/07_for.tcl
+++ Concepts/07_for.tcl
@@ -1,5 +1,6 @@
 #! /bin/tclsh8.5
 
+#example_1
 for {set i 0} {$i <= 10} {incr i} {
 
 	if {$i == 6} {
@@ -17,3 +18,27 @@ puts $i
 3
 4
 5
+
+
+
+#example_2
+for {set i 0} {$i < 10} {incr i} {
+
+	if {$i == 6} {
+ 	continue
+  	}
+
+puts $i
+}
+
+
+#Output:
+0
+1
+2
+3
+4
+5
+7
+8
+9
