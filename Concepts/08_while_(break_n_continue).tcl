a59495d2456a96ea996abb158876abc0b8007451
Manish Mani <manishpamu@hotmail.com>
2025-01-30 22:43:34 +0530

    Update 08_while_(break_n_continue).tcl

--- Concepts/08_while_(break_n_continue).tcl
+++ Concepts/08_while_(break_n_continue).tcl
@@ -1,1 +1,21 @@
 #! /bin/tclsh8.5
+
+set j 1
+
+while {$j <= 10} {
+
+	set k 1
+ 	while {$k <= 3} {
+	
+		if {$k == 2} {
+  		incr k
+    		continue
+		}
+
+      		else {
+		puts "$j ---- $k"
+  		incr k
+		}
+	}
+
+}
