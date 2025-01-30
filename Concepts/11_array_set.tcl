04eff362b2f84782c7cbcb5e74c0c46bbdb44b7d
Manish Mani <manishpamu@hotmail.com>
2025-01-30 22:29:03 +0530

    Update 11_array_set.tcl

--- Concepts/11_array_set.tcl
+++ Concepts/11_array_set.tcl
@@ -1,1 +1,21 @@
 #! /bin/tclsh8.5
+
+array set maypd24 {
+
+	names {candid_1 candid_2 candid_3}
+ 	id {MAYPD001 MAYPD002 MAYPD003}
+	phone {1111 2222 3333}
+ 
+}
+
+puts "[lindex $maypd24(names) 0]";		#Output: candid_1
+puts "[lindex $maypd24(id) 0]";			#Output: MAYPD001
+puts "[lindex $maypd24(phone) 0]";		#Output: 1111
+
+puts [array size maypd24];			#Output: 3
+
+#by default the array will get sorted alphabetical order so we will ge 'id' list first 
+puts [lindex [array get maypd24] 0];		#Output: id
+
+puts [array exists maypd24];			#Output: 1
+puts [array exists maypd2024];			#Output: 0
