ae814690393e7d476ef98682a86db79f9674cc60
Manish Mani <manishpamu@hotmail.com>
2025-01-30 19:20:09 +0530

    Update 06_b_foreach.tcl

--- Concepts/06_b_foreach.tcl
+++ Concepts/06_b_foreach.tcl
@@ -1,1 +1,21 @@
 #! /bin/tclsh8.5
+
+puts -nonewline "Enter Logic Gate Names (ex: AND OR NOT): "
+flush stdout
+gets stdin gates
+
+foreach i $gates {
+
+	switch $i {
+
+ 	AND { puts "AND_Operation" }
+  	OR { puts "OR_operation" }
+   	NOT { puts "NOT_operation" }
+    	NAND { puts "NAND_operation" }
+     	NOR { puts "NOR_operation" }
+      	XOR { puts "XOR_operation" }
+       	XNOR { puts "XNOR_operation" }
+	
+	}
+
+}
