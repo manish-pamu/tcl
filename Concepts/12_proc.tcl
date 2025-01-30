c783a99caeb94f105ecd6e8c3642e0f83279b43c
Manish Mani <manishpamu@hotmail.com>
2025-01-30 19:37:19 +0530

    Update 12_proc.tcl

--- Concepts/12_proc.tcl
+++ Concepts/12_proc.tcl
@@ -1,5 +1,12 @@
 #! /bin/tclsh8.5
+#benefits of proc: code reusability and runtime reduction.
 
-#code reusability
-#runtime reduction
 
+#procedure definition
+proc add {a b} {
+return [expr $a + $b]
+}
+
+#procedure call
+puts "[add 5 5]";		#Output: 10
+puts "[add 10 10]";		#Output: 20
