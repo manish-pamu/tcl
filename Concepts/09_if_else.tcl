fc04c2d120acf9d98002a260c4d562162282d30a
Manish Mani <manishpamu@hotmail.com>
2025-01-30 19:06:12 +0530

    Update 09_if_else.tcl

--- Concepts/09_if_else.tcl
+++ Concepts/09_if_else.tcl
@@ -1,1 +1,16 @@
 #! /bin/tclsh8.5
+
+puts -nonewline "Enter a number"
+flush stdout
+gets stdin num
+
+if {$num > 0} { 
+puts "The $num is positive" 
+
+	elseif {$num < 0} {
+ 	puts "The $num is negative"	
+  	}
+  
+else { puts "The $num is neutral" }
+
+}
