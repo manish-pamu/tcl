6286aed76ca806a9028367a0355e488570f2530f
Manish Mani <manishpamu@hotmail.com>
2025-01-30 19:10:10 +0530

    Update 06_foreach.tcl

--- Concepts/06_foreach.tcl
+++ Concepts/06_foreach.tcl
@@ -1,2 +1,21 @@
 #! /bin/tclsh8.5
 
+puts -nonewline "Enter any numbers: "
+flush stdout
+gets stdin num
+
+foreach i $num {
+
+	if {$i > 0} { 
+	puts "The $i is positive" 
+
+		elseif {$i < 0} {
+ 		puts "The $i is negative"	
+  		}
+  
+	else { puts "The $i is neutral" }	
+	}
+
+}
+
+
