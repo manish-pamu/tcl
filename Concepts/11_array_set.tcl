1e90b1046059f7f868577712ac7cba0824f71049
Manish Mani <manishpamu@hotmail.com>
2025-01-30 22:35:34 +0530

    Update 11_array_set.tcl

--- Concepts/11_array_set.tcl
+++ Concepts/11_array_set.tcl
@@ -8,6 +8,10 @@ array set maypd24 {
  
 }
 
+puts "$paypd24(names)";				#Output: candid_1 candid_2 candid_3
+puts "$paypd24(id)";				#Output: MAYPD001 MAYPD002 MAYPD003
+puts "$paypd24(phone)";				#Output: 1111 2222 3333
+
 puts "[lindex $maypd24(names) 0]";		#Output: candid_1
 puts "[lindex $maypd24(id) 0]";			#Output: MAYPD001
 puts "[lindex $maypd24(phone) 0]";		#Output: 1111
