07ac3d0d791c79a952cd92ff9d235ad387f9d3f8
Manish Mani <manishpamu@hotmail.com>
2025-02-05 20:27:41 +0530

    Update 7_Elapsed_time_v3_using_proc.tcl

--- TCL Scripts/7_Elapsed_time_v3_using_proc.tcl
+++ TCL Scripts/7_Elapsed_time_v3_using_proc.tcl
@@ -1,2 +1,33 @@
 #! /bin/tclsh
 
+#procedure to calculate the elapsed time
+proc elapsed_time {command} {
+
+	set start_time [clock clicks -milliseconds]
+	eval $command
+ 	set start_time [clock clicks -milliseconds]
+
+	set elapsed_time [expr $end_time - $start_time]
+ 	return $elapsed_time
+	
+}
+
+#read the file containing commands in reading mode
+set lines [open "7_ex_rndm_commands.tcl" r]
+set commands [split [read $lines] \n]
+close $lines
+
+#iterate over each command and call the procedure
+foreach command $commands {
+
+	if {[string trim command] ne ""} {
+
+  		#here we are calling the procedure and storing the returned value in variable
+		set elapsed_time_ms [elapsed_time $command]
+  		puts "#########################################################"
+    		puts "# Elapsed Time		: $elapsed_time_ms"
+      		puts "# Command Executed	: $command"
+     		puts "#########################################################"
+	}
+
+}
