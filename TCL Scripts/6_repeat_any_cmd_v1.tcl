735964c1ed4975eb37e3d0ae2a41d56db580c06e
Manish Mani <manishpamu@hotmail.com>
2025-02-05 20:47:50 +0530

    Create 6_repeat_any_cmd_v1.tcl

---
+++ TCL Scripts/6_repeat_any_cmd_v1.tcl
@@ -0,0 +1,16 @@
+#procedure
+proc repeat {count body} {
+
+	#for "count" number of times execute the code_snippet/block_of_code that is stored in body variable
+ 	for {set i 1} {$i <= $count} {incr i} {
+
+	eval body;
+ 	#or
+  	uplevel $body
+   	#both are same
+    	
+	}
+
+}
+
+repeat 5 { puts "Hello...repeat" }
