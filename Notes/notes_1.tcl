8522e0cb31dd243066c900b00dd7a908dd0872d8
Manish Mani <manishpamu@hotmail.com>
2025-06-25 18:42:37 +0530

    Update notes_1.tcl

--- Notes/notes_1.tcl
+++ Notes/notes_1.tcl
@@ -1,7 +1,19 @@
-#get the no. of logiclevels in a timing path from report_timing
-
+##########################################################################
+#AIM : To get the no. of logiclevels in a timing path from report_timing
+##########################################################################
 #1. load the applet
 applet load get_logic_levels
 
 #run the command "report_timing" with start and end points
 get_logic_levels "report_timing -from <startpoint> -to <endpoint>"
+
+
+##########################################################################
+#AIM : 
+##########################################################################
+
+
+
+
+
+
