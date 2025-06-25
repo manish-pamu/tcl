79c63df436fc0a7bb3f55437c033d386aa22a496
Manish Mani <manishpamu@hotmail.com>
2025-06-25 16:30:33 +0530

    Create notes_1.tcl

---
+++ Notes/notes_1.tcl
@@ -0,0 +1,7 @@
+#get the no. of logiclevels in a timing path from report_timing
+
+#1. load the applet
+applet load get_logic_levels
+
+#run the command "report_timing" with start and end points
+get_logic_levels "report_timing -from <startpoint> -to <endpoint>"
